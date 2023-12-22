d29fcd2cdbf86dda6cac23cdf4954e42d7a61bd8
warnshouse <warnshouse@protonmail.com>
2023-12-22 20:02:14 +0000

    Add solution to Day 22 challenge

---
+++ 2023/Day-22/index.js
@@ -0,0 +1,54 @@
+let people = JSON.parse(localStorage.getItem("list")) || [];
+
+const inputFieldEl = document.getElementById("input-field");
+const addButtonEl = document.getElementById("add-button");
+const peopleListEl = document.getElementById("people-list");
+const imageEl = document.getElementById("image");
+
+if (people.length) {
+  renderList(JSON.parse(localStorage.getItem("list")));
+}
+
+addButtonEl.addEventListener("click", function () {
+  let inputValue = inputFieldEl.value;
+    
+  if (inputValue) {
+    people.push(inputValue);
+    clearInputFieldEl();
+    localStorage.setItem("list", JSON.stringify(people));
+    renderList(JSON.parse(localStorage.getItem("list")));
+    imageEl.src = "icon.png";
+  }
+});
+
+function renderList(array) {
+  clearPeopleListEl();
+    
+  for (let i = 0; i < array.length; i++) {
+    let currentPerson = array[i];
+    appendPersonToPeopleListEl(currentPerson);
+  }
+}
+
+function clearPeopleListEl() {
+  peopleListEl.innerHTML = "";
+}
+
+function clearInputFieldEl() {
+  inputFieldEl.value = "";
+}
+
+function appendPersonToPeopleListEl(person) {
+  let newEl = document.createElement("li");
+  newEl.textContent = person;
+    
+  newEl.addEventListener("dblclick", function () {
+    let index = people.indexOf(person);
+    people.splice(index, 1);
+    localStorage.setItem("list", JSON.stringify(people));
+    renderList(JSON.parse(localStorage.getItem("list")));
+    imageEl.src = "https://media.tenor.com/-P2O6WcsxNgAAAAM/the-critic-jon-lovitz.gif";
+  });
+
+  peopleListEl.append(newEl);
+}
